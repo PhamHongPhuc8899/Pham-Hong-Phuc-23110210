6e72c1e3cf2c3a66a3bcc25517b3ae5e81a15ba5
Pham Hong Phuc <phamhongphuc1188@gmail.com>
2026-04-22 08:12:25 +0700

    update code

--- bai2.py
+++ bai2.py
@@ -2,9 +2,7 @@ def chuan_hoa_ten(name):
     """
     Chuẩn hóa chuỗi: bỏ khoảng trắng thừa và viết hoa chữ cái đầu mỗi từ.
     """
-    # split() không tham số sẽ tự động tách theo mọi khoảng trắng và loại bỏ khoảng trắng thừa
     cac_tu = name.split()
-    # Viết hoa chữ cái đầu của từng từ và nối lại bằng một khoảng trắng duy nhất
     ten_chuan_hoa = " ".join(tu.capitalize() for tu in cac_tu)
     return ten_chuan_hoa
 
@@ -12,7 +10,6 @@ def sap_xep_theo_ten(danh_sach):
     """
     Sắp xếp danh sách dựa trên Tên (từ cuối cùng trong chuỗi).
     """
-    # Sử dụng hàm sorted với key là từ cuối cùng của chuỗi đã chuẩn hóa
     danh_sach_sap_xep = sorted(danh_sach, key=lambda x: x.split()[-1])
     return danh_sach_sap_xep
 
